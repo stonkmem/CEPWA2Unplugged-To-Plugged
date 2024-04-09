aee0e08a467a0cb2f7723f81824ef9deb65f1383
stonkmem <73430589+stonkmem@users.noreply.github.com>
2024-04-09 08:09:53 +0000

    l[oijhuytfndkj oighrtejaoidlfs
    hy5ejtgfbd

--- class.js
+++ class.js
@@ -1,127 +1,154 @@
 class twoBlock {
-  constructor(V, occI, col) {
-    this.vert = V;
-    this.occ = occI;
-    this.color = col;
-    this.sel = false; // SELECTED
-    this.occ.sort();
-  }
-  moveR() {
-    if (this.vert == false && this.sel == true && ledger[this.occ[1]+1] == 0){
-      for (let i = 0; i < this.occ.length; i++) {
-          this.occ[i] += 1;
+    constructor(V, occI, col) {
+        this.vert = V;
+        this.occ = occI;
+        this.color = col;
+        this.sel = false; // SELECTED
+        this.occ.sort();
+    }
+    moveR() {
+        if (this.vert == false && this.sel == true && ledger[this.occ[1] + 1] == 0 && this.occ[1] % 6 != 5) {
+            for (let i = 0; i < this.occ.length; i++) {
+                this.occ[i] += 1;
+            }
+        }
+        console.log(this.vert, this.sel, this.occ, ledger[this.occ[1] + 1]);
+    }
+
+    moveL() {
+        if (this.vert == false && this.sel == true && this.occ[0] != 0 && ledger[this.occ[0] - 1] == 0 && this.occ[0] % 6 != 0) {
+            for (let i = 0; i < this.occ.length; i++) {
+                this.occ[i] -= 1;
+            }
+        }
+        console.log(this.vert, this.sel, this.occ, ledger[this.occ[0] - 1] == 0);
+    }
+
+    moveU() {
+        if (this.vert == true && this.sel == true && ledger[this.occ[0] - 6] == 0 && this.occ[0] > 5) {
+            for (let i = 0; i < this.occ.length; i++) {
+                this.occ[i] -= 6;
+            }
         }
     }
-  }
 
-  moveL() {
-    if (this.vert == false && this.sel == true && ledger[this.occ[0]-1] == 0){
-      for (let i = 0; i < this.occ.length; i++) {
-        this.occ[i]-= 1;
-      }
-    }
-  }
-
-  moveU() {
-    if (this.vert == true && this.sel == true && ledger[this.occ[0]-6] == 0){
-      for (let i = 0; i < this.occ.length; i++) {
-        this.occ[i]-= 6;
-      }
-    }
-  }
-
-  moveD() {
-    if (this.vert == false && this.sel == true && ledger[this.occ[1]+6] == 0){
-      for (let i = 0; i < this.occ.length; i++) {
-        this.occ[i]+= 6;
-      }
-    }
-  }
-
-  epoch() {
-    for (let i = 0; i < this.occ.length; i++) {
-        rectMode(RADIUS);
-        fill(this.color);
-        if(this.sel === false){
-            rect(squareToCords(this.occ[i]).x, squareToCords(this.occ[i]).y, 50, 50);
-        }
-        if(this.sel === true){
-            rect(squareToCords(this.occ[i]).x, squareToCords(this.occ[i]).y, 60, 60);
+    moveD() {
+        if (this.vert == true && this.sel == true && ledger[this.occ[1] + 6] == 0 && this.occ[1] < 30) {
+            for (let i = 0; i < this.occ.length; i++) {
+                this.occ[i] += 6;
+            }
         }
     }
-  }
+
+    epoch() {
+        for (let i = 0; i < this.occ.length; i++) {
+            rectMode(RADIUS);
+            fill(this.color);
+            if (this.sel === false) {
+                rect(squareToCords(this.occ[i]).x, squareToCords(this.occ[i]).y, 50, 50);
+            }
+            if (this.sel === true) {
+                rect(squareToCords(this.occ[i]).x, squareToCords(this.occ[i]).y, 60, 60);
+            }
+        }
+    }
 }
 
 class threeBlock {
-  constructor(V, occI, col) {
-    this.vert = V;
-    this.occ = occI;
-    this.color = col;
-    this.sel = false; // SELECTED
-    this.occ.sort();
-  }
-  moveR() {
-    if (this.vert == false && this.sel == true && ledger[this.occ[2]+1] == 0){
-      for (let i = 0; i < this.occ.length; i++) {
-          this.occ[i] += 1;
-      }
+    constructor(V, occI, col) {
+        this.vert = V;
+        this.occ = occI;
+        this.color = col;
+        this.sel = false; // SELECTED
+        this.occ.sort();
     }
-  }
+    moveR() {
+        if (this.vert == false && this.sel == true && ledger[this.occ[2] + 1] == 0 && this.occ[2] % 6 != 5) {
+            for (let i = 0; i < this.occ.length; i++) {
+                this.occ[i] += 1;
+            }
+        }
+    }
 
-  moveL() {
-    if (this.vert == false && this.sel == true && ledger[this.occ[0]-1] == 0){
-      for (let i = 0; i < this.occ.length; i++) {
-        this.occ[i]-= 1;
-      }
+    moveL() {
+        if (this.vert == false && this.sel == true && ledger[this.occ[0] - 1] == 0 && this.occ[0] % 6 != 0) {
+            for (let i = 0; i < this.occ.length; i++) {
+                this.occ[i] -= 1;
+            }
+        }
     }
-  }
 
-  moveU() {
-    if (this.vert == true && this.sel == true && ledger[this.occ[0]-6] == 0){
-      for (let i = 0; i < this.occ.length; i++) {
-        this.occ[i]-= 6;
-      }
+    moveU() {
+        if (this.vert == true && this.sel == true && ledger[this.occ[0] - 6] == 0 && this.occ[0] > 5) {
+            for (let i = 0; i < this.occ.length; i++) {
+                this.occ[i] -= 6;
+            }
+        }
     }
-  }
 
-  moveD() {
-    if (this.vert == false && this.sel == true && ledger[this.occ[2]+6] == 0){
-      for (let i = 0; i < this.occ.length; i++) {
-        this.occ[i]+= 6;
-      }
+    moveD() {
+        if (this.vert == true && this.sel == true && ledger[this.occ[2] + 6] == 0 && this.occ[2] < 30) {
+            for (let i = 0; i < this.occ.length; i++) {
+                this.occ[i] += 6;
+            }
+        }
     }
-  }
 
-  epoch() {
-    for (let i = 0; i < this.occ.length; i++) {
-      rectMode(RADIUS);
-      fill(this.color);
-      if(this.sel === false){
-          rect(squareToCords(this.occ[i]).x, squareToCords(this.occ[i]).y, 50, 50);
-      }
-      if(this.sel === true){
-          rect(squareToCords(this.occ[i]).x, squareToCords(this.occ[i]).y, 60, 60);
-      }
+    epoch() {
+        for (let i = 0; i < this.occ.length; i++) {
+            rectMode(RADIUS);
+            fill(this.color);
+            if (this.sel === false) {
+                rect(squareToCords(this.occ[i]).x, squareToCords(this.occ[i]).y, 50, 50);
+            }
+            if (this.sel === true) {
+                rect(squareToCords(this.occ[i]).x, squareToCords(this.occ[i]).y, 60, 60);
+            }
+        }
     }
-  }
 }
 
 function coordsToSquare(x, y) {
     if (x > 600 || y > 600) {
-      return -1;
+        return -1;
     }
     return 6 * (Math.floor(y / 100)) + Math.floor(x / 100);
 }
 
 function squareToCords(i) {
-  return createVector(50 + (i % 6) * 100, 50 + Math.floor(i / 6) * 100);
+    return createVector(50 + (i % 6) * 100, 50 + Math.floor(i / 6) * 100);
 }
 
 function mousePressed() {
-  let mind = coordsToSquare(mouseX, mouseY);
-  console.log(coordsToSquare(mouseX, mouseY));
-  if (ledger[mind] != 0) {
-      focus=ledger[mind];
-      cars[focus].sel = true;
-  }
+    let mind = coordsToSquare(mouseX, mouseY);
+    if (ledger[mind] != 0 && cars[ledger[mind] - 1].sel == true) {
+        cars[focus - 1].sel = false;
+    }
+    else {
+        if (mind < 0) mind = 0;
+        // console.log(focus);
+        if (focus != 0) {
+            cars[focus - 1].sel = false;
+        }
+        // console.log(ledger[mind]);
+        focus = ledger[mind];
+        if (ledger[mind] != 0) {
+            cars[focus - 1].sel = true;
+        }
+    }
+}
+
+function keyPressed() {
+    if (keyCode === RIGHT_ARROW) {
+        cars[focus - 1].moveR();
+    }
+    if (keyCode === LEFT_ARROW) {
+        cars[focus - 1].moveL();
+    }
+    if (keyCode === UP_ARROW) {
+        cars[focus - 1].moveU();
+    }
+    if (keyCode === DOWN_ARROW) {
+        cars[focus - 1].moveD();
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,48 +1,69 @@
-let cars = [], ledger = [], focus = 0;
+let cars = [], ledger = [], focus = 0, goal = 17;
 
 function setup() {
-  createCanvas(600, 600);
-  cars.push(new twoBlock(true, [1, 2], color(56, 90, 255)));
-  for (let i = 0; i < 36; i++) {
-      ledger.push(0);
-  }
+    createCanvas(600, 600);
+    cars.push(new twoBlock(false, [1, 2], color("red")));
+    for (let i = 0; i < 36; i++) {
+        ledger.push(0);
+    }
 
-  for (let i = 0; i < cars.length; i++){
-      for(let j=0; j < cars[i].occ.length; j+=1){
-          ledger[cars[i].occ[j]] = i+1;
-      }
-  }
+    cars.push(new threeBlock(true, [23, 29, 35], color(56, 90, 255)));
+    for (let i = 0; i < 36; i++) {
+        ledger.push(0);
+    }
 
-  for(let i=0; i<ledger.length; i+=1){
-      console.log(ledger[i]);
-  }
+    for (let i = 0; i < cars.length; i++) {
+        for (let j = 0; j < cars[i].occ.length; j += 1) {
+            ledger[cars[i].occ[j]] = i + 1;
+        }
+    }
+
+    for (let i = 0; i < ledger.length; i += 1) {
+        console.log(ledger[i]);
+    }
 }
 
 function draw() {
-  background("lightblue")
+    background("lightblue")
 
-  //LAYOUT AND AESTHETICS
-  strokeWeight(1)
-  //horizontal grid lines
-  line(0, height * 1 / 6, width, height * 1 / 6)
-  line(0, height * 2 / 6, width, height * 2 / 6)
-  line(0, height * 3 / 6, width, height * 3 / 6)
-  line(0, height * 4 / 6, width, height * 4 / 6)
-  line(0, height * 5 / 6, width, height * 5 / 6)
+    //check for win
+    for(let i = 0; i < cars[0].occ.length; i += 1){
+        if(cars[0].occ[i] == goal){
+            console.log(":)");
+        }
+    }
 
-  //vertical grid lines
-  line(width * 1 / 6, 0, width * 1 / 6, height)
-  line(width * 2 / 6, 0, width * 2 / 6, height)
-  line(width * 3 / 6, 0, width * 3 / 6, height)
-  line(width * 4 / 6, 0, width * 4 / 6, height)
-  line(width * 5 / 6, 0, width * 5 / 6, height)
+    //LAYOUT AND AESTHETICS
+    strokeWeight(1)
+    //horizontal grid lines
+    line(0, height * 1 / 6, width, height * 1 / 6)
+    line(0, height * 2 / 6, width, height * 2 / 6)
+    line(0, height * 3 / 6, width, height * 3 / 6)
+    line(0, height * 4 / 6, width, height * 4 / 6)
+    line(0, height * 5 / 6, width, height * 5 / 6)
 
-  //exit zone
-  strokeWeight(0)
-  fill("red")
-  rect(width - 10, height * 1 / 6, 10, 100)
+    //vertical grid lines
+    line(width * 1 / 6, 0, width * 1 / 6, height)
+    line(width * 2 / 6, 0, width * 2 / 6, height)
+    line(width * 3 / 6, 0, width * 3 / 6, height)
+    line(width * 4 / 6, 0, width * 4 / 6, height)
+    line(width * 5 / 6, 0, width * 5 / 6, height)
 
-  for(let i = 0; i<cars.length; i+=1){
-      cars[i].epoch();
-  }
+    //exit zone
+    strokeWeight(0)
+    fill("red")
+    rect(width, height * 3/6, 10, 100)
+
+    for (let i = 0; i < 36; i += 1) {
+        ledger[i] = 0;
+    }
+    for (let i = 0; i < cars.length; i++) {
+        for (let j = 0; j < cars[i].occ.length; j += 1) {
+            ledger[cars[i].occ[j]] = i + 1;
+        }
+    }
+
+    for (let i = 0; i < cars.length; i += 1) {
+        cars[i].epoch();
+    }
 }
